352aa6700664cdafb4f52f4c0da8731a5cbfd10d
白云辉 <baiyh@weipaitang.com>
2020-10-12 13:05:48 +0800

    add add version and test

--- process_test.go
+++ process_test.go
@@ -3,37 +3,63 @@ package xprocess_test
 import (
 	"context"
 	"fmt"
+	"github.com/pubgo/xerror"
 	"github.com/pubgo/xprocess"
 	"testing"
 	"time"
 )
 
+func TestCancel(t *testing.T) {
+	fmt.Println(xprocess.Stack())
+	cancel := xprocess.Go(func(ctx context.Context) error {
+		for {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			default:
+				time.Sleep(time.Second)
+				fmt.Println("g1")
+			}
+		}
+	})
+
+	time.Sleep(time.Second*5)
+	fmt.Println(xprocess.Stack())
+	fmt.Println(cancel())
+	time.Sleep(time.Millisecond*10)
+	fmt.Println(xprocess.Stack())
+}
 func TestName(t *testing.T) {
 	fmt.Println(xprocess.Stack())
 	for {
 		xprocess.Go(func(ctx context.Context) error {
 			time.Sleep(time.Second)
-
-			return ctx.Err()
-		})
-		xprocess.Go(func(ctx context.Context) error {
-			time.Sleep(time.Second)
-
+			fmt.Println("g2")
 			return ctx.Err()
 		})
 		xprocess.GoLoop(func(ctx context.Context) error {
-			time.Sleep(time.Millisecond * 10)
-			fmt.Println("ok")
+			time.Sleep(time.Second)
+			fmt.Println("g3")
 			return ctx.Err()
 		})
 
 		g := xprocess.NewGroup(nil)
 		g.Go(func(ctx context.Context) error {
-
+			fmt.Println("g4")
 			return nil
 		})
+		g.Go(func(ctx context.Context) error {
+			fmt.Println("g5")
+			return nil
+		})
+		g.Go(func(ctx context.Context) error {
+			fmt.Println("g6")
+			return xerror.Fmt("test error")
+		})
 		g.Wait()
+		fmt.Println(g.Err())
 
 		fmt.Println(xprocess.Stack())
+		time.Sleep(time.Second)
 	}
 }
